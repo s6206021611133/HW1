df52f1e775e704ff944a32d6b339cb98b0fc2e0d
s6206021611133 <76947836+s6206021611133@users.noreply.github.com>
2021-01-13 13:41:01 +0700

    Add files via upload

---
+++ HomeWork1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	cout << " 1st Term of 2020 \t \\6206021611133  Suphachock Saengbodee "<< endl ;
+	cout << endl ;
+	cout << " ----------------------------------------------------------------------- " << endl ;
+	cout << " | Subject ID | Subject name             | Score (100) | Grad | \n"  ;
+	cout << " ----------------------------------------------------------------------- " << endl ;
+	cout << " |   4570034  | Introduction to computer |      85     |  'A' | \n"  ;
+	cout << " |   4580247  | English                  |      74     |  'B' | \n"  ;
+	cout << " |   4530001  | Design Thinking          |      92     |  'A' | \n"  ;
+	cout << " ----------------------------------------------------------------------- " << endl ;
+	cout << " |   Total    | Subject = 3              | Average = 3.87     | \n"  ; 
+	cout << " ===================================*=== " ;
+	cout << endl ;
+	cout << endl ;
+	cout << endl ;
+	cout << "KMUTNB"<<endl ;
+	return 0 ;
+}
